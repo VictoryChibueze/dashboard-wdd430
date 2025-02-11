2d8292b9a069e45abd2cb72536fd47f73090792b
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-02-11 12:39:52 +0100

    wk03

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,12 +2,17 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/font";
-import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import CardWrapper from "@/app/ui/dashboard/cards";
+import { fetchCardData } from "@/app/lib/data"; // Remove fetchLatestInvoices
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
+  // Remove `const latestInvoices = await fetchLatestInvoices()`
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -34,7 +39,12 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
     </main>
   );
